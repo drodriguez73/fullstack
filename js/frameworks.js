00e4861ff97b782dfd91339ce583ba5fd810b372
Diego Rodriguez <tuprogramador@yahoo.com.ar>
2018-12-30 12:01:57 -0300

    documentacion inicial

---
+++ js/frameworks.js
@@ -0,0 +1,24 @@
+Graphql.js
+	A query language for your API
+	https://graphql.org/
+	
+
+Express.js
+	Infraestructura web rápida, minimalista y flexible para Node.js
+	https://expressjs.com/es/
+	
+Node.js
+	Es un entorno de ejecución para JavaScript construido con el motor de JavaScript V8 de Chrome.
+	https://nodejs.org/es/
+	
+React.js
+	A JavaScript library for building user interfaces
+	https://reactjs.org/
+	
+Vue.js
+	The Progressive JavaScript Framework 
+	https://vuejs.org/
+	
+Redux
+	Is a predictable state container for JavaScript apps.
+	https://redux.js.org/
